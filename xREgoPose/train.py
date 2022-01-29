00ec83c9c1278809e7d1580d4c6fcdc01332dbc1
Jinman Park <jmpark0808@gmail.com>
2022-01-29 16:03:56 -0500

    Tweaking optimizer

--- xREgoPose/train.py
+++ xREgoPose/train.py
@@ -137,8 +137,10 @@ if __name__ == '__main__':
     decay_step = args.decay_step
     learning_rate_hm = learning_rate_hm * (lr_decay ** (start_iter // decay_step))
     learning_rate_pose = learning_rate_pose * (lr_decay ** (start_iter // decay_step))
-    opt_hm = torch.optim.AdamW(model_hm.parameters(), lr=learning_rate_hm, weight_decay=0.01)
-    opt_pose = torch.optim.AdamW(model_pose.parameters(), lr=learning_rate_pose, weight_decay=0.01)
+    #opt_hm = torch.optim.AdamW(model_hm.parameters(), lr=learning_rate_hm, weight_decay=0.01)
+    #opt_pose = torch.optim.AdamW(model_pose.parameters(), lr=learning_rate_pose, weight_decay=0.01)
+    opt_hm = torch.optim.SGD(model_hm.parameters(), lr=learning_rate_hm, momentum=0.9, weight_decay=0.0005)
+    opt_pose = torch.optim.AdamW(model_pose.parameters(), lr=learning_rate_pose, momentum=0.9, weight_decay=0.0005)
 
     # Logger Setup
     os.makedirs(os.path.join('log', now.strftime('%m%d%H%M')), exist_ok=True)
