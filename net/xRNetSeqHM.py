a0a74ddd082c9a9770dd6b43ef71994c9cdc2df3
Jinman Park <jmpark0808@gmail.com>
2022-02-25 14:48:08 -0500

    LR scheduler bug

--- net/xRNetSeqHM.py
+++ net/xRNetSeqHM.py
@@ -98,8 +98,10 @@ class xREgoPoseSeqHM(pl.LightningModule):
         """
         
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)
-        scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=0.00000001, end_factor=1.0, total_iters=int(self.hm_train_steps/self.batch_size))
-
+        shceduler = {'scheduler': torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=0.00000001, end_factor=1.0, total_iters=int(self.hm_train_steps/self.batch_size)),
+                        'name': 'learning_rate',
+                        'interval':'step',
+                        'frequency': 1}
         return [optimizer], [scheduler]
 
     def forward(self, x):
