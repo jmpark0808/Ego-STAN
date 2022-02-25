fd235d3cd6a97cf07b329035c308c82a66efd244
Jinman Park <jmpark0808@gmail.com>
2022-02-25 08:26:37 -0500

    Seq HM loss reshape instead of view

--- net/xRNetSeqHM.py
+++ net/xRNetSeqHM.py
@@ -87,7 +87,7 @@ class xREgoPoseSeqHM(pl.LightningModule):
         cos = torch.nn.CosineSimilarity(dim=2, eps=1e-6)
         cosine_similarity_error = torch.sum(cos(pose_pred, pose_label), dim=1)
         limb_length_error = torch.sum(torch.sum(torch.abs(pose_pred-pose_label), dim=2), dim=1)
-        heatmap_error = torch.sqrt(torch.sum(torch.pow(hm_resnet.view(hm_resnet.size(0), -1) - hm_decoder.view(hm_decoder.size(0), -1), 2), dim=1))
+        heatmap_error = torch.sqrt(torch.sum(torch.pow(hm_resnet.reshape(hm_resnet.size(0), -1) - hm_decoder.reshape(hm_decoder.size(0), -1), 2), dim=1))
         LAE_pose = lambda_p*(pose_l2norm + lambda_theta*cosine_similarity_error + lambda_L*limb_length_error)
         LAE_hm = lambda_hm*heatmap_error
         return torch.mean(LAE_pose), torch.mean(LAE_hm)
