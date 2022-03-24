f9657ee61f5310a5e1f9ff7d0b60452748e89d86
Jinman Park <jmpark0808@gmail.com>
2022-03-23 20:21:40 -0400

    Change optimizer

--- net/xRNetUNet.py
+++ net/xRNetUNet.py
@@ -85,8 +85,8 @@ class xREgoPoseUNet(pl.LightningModule):
         Choose what optimizers and learning-rate schedulers to use in your optimization.
         """
         
-        optimizer = torch.optim.SGD(
-        self.parameters(), lr=self.lr, momentum=0.9, nesterov=True
+        optimizer = torch.optim.AdamW(
+        self.parameters(), lr=self.lr
         )
         
         return optimizer
