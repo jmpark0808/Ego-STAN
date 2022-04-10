01342de28af6310263bfcc6db6368a194419ee86
Jinman Park <jmpark0808@gmail.com>
2022-04-09 20:52:07 -0400

    Change 100 to 1000

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -10,14 +10,14 @@ from matplotlib.pyplot import bone
 from mpl_toolkits.mplot3d.axes3d import get_test_data
 
 import numpy as np
-from base import BaseEval
+# from base import BaseEval
 import scipy.io
 import os
 
 __all__ = ["EvalBody", "EvalUpperBody", "EvalLowerBody"]
 
-mean3D = scipy.io.loadmat(os.path.join(os.path.expanduser('~'), 'projects/def-pfieguth/mo2cap/code/util/mean3D.mat'))['mean3D']/100. # 3x15 shape
-# mean3D = scipy.io.loadmat('/home/eddie/scripts/code/util/mean3D.mat')['mean3D']/100.
+mean3D = scipy.io.loadmat(os.path.join(os.path.expanduser('~'), 'projects/def-pfieguth/mo2cap/code/util/mean3D.mat'))['mean3D']/1000. # 3x15 shape
+# mean3D = scipy.io.loadmat('/home/eddie/scripts/code/util/mean3D.mat')['mean3D']/1000.
 kinematic_parents = [ 0, 0, 1, 2, 0, 4, 5, 1, 7, 8, 9, 4, 11, 12, 13]
 bones_mean = mean3D - mean3D[:,kinematic_parents]
 bone_length = np.sqrt(np.sum(np.power(bones_mean, 2), axis=0)) # 15 shape
