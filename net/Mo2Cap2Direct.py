5fc2e7562c19cf056132d21f49f57cc0f5c21efb
Jinman Park <jmpark0808@gmail.com>
2022-04-27 22:33:25 -0400

    Change optimizer to adamw

--- net/Mo2Cap2Direct.py
+++ net/Mo2Cap2Direct.py
@@ -115,7 +115,7 @@ class Mo2Cap2Direct(pl.LightningModule):
             {"params": self.pose.parameters()},
         ]
 
-        optimizer = torch.optim.Adadelta(
+        optimizer = torch.optim.AdamW(
         grouped_parameters, lr=self.lr
         )
         self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
